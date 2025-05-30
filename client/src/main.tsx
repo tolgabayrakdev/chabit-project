7a184e96b17f8fdfd20590b9d0e69a186747fc7d
Tolga BAYRAK <bayraktolga28@gmail.com>
2025-05-30 14:07:21 +0300

    mantine ui added for project component

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,12 +1,19 @@
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router'
 import './index.css'
+import '@mantine/core/styles.css';
+import { MantineProvider } from '@mantine/core';
+
+
 import Home from './pages/Home'
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Home />} />
-    </Routes>
-  </BrowserRouter>,
+  <MantineProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  </MantineProvider>
+
 )
