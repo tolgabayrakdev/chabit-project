84dae0bd165b546a0a6c1b4d5093846e63cd72a4
Tolga BAYRAK <bayraktolga28@gmail.com>
2025-06-07 20:33:05 +0300

    organization

--- node-server/src/routes/qr-routes.js
+++ node-server/src/routes/qr-routes.js
@@ -7,11 +7,11 @@ import { verifyToken } from "../middlewares/verify-token.js";
 const router = express.Router();
 const qrController = new QRController();
 
-router.post("/generate-sms-qr", verifyToken, qrController.generateSmsQrCode.bind(qrController));
-router.post("/generate-mail-qr", verifyToken, qrController.generateMailQrCode.bind(qrController));
-router.post("/generate-wifi-qr", verifyToken, qrController.generateWifiQrCode.bind(qrController));
-router.post("/generate-vcard-qr", verifyToken, qrController.generateVCardQrCode.bind(qrController));
-router.post("/generate-url-qr", verifyToken, qrController.generateUrlQrCode.bind(qrController));
+router.post("/sms", verifyToken, qrController.generateSmsQrCode.bind(qrController));
+router.post("/mail", verifyToken, qrController.generateMailQrCode.bind(qrController));
+router.post("/wifi", verifyToken, qrController.generateWifiQrCode.bind(qrController));
+router.post("/vcard", verifyToken, qrController.generateVCardQrCode.bind(qrController));
+router.post("/url", verifyToken, qrController.generateUrlQrCode.bind(qrController));
 router.get("/:id", verifyToken, qrController.getQrCode.bind(qrController));
 router.get("/", verifyToken, qrController.listQrCodes.bind(qrController));
 router.delete("/:id", verifyToken, qrController.deleteQrCode.bind(qrController));
