7defd1b369af34aa4e6a20f5d8af05eec7e0d6cb
Tolga BAYRAK <bayraktolga28@gmail.com>
2025-06-10 23:39:28 +0300

    loading component

---
+++ web/app/dashboard/loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function loading() {
+    return (
+        <div className="flex h-screen items-center justify-center">
+            <div className="relative">
+                <div className="animate-spin rounded-full h-6 w-6 border-2 border-t-transparent border-black"></div>
+            </div>
+        </div>
+    )
+}
