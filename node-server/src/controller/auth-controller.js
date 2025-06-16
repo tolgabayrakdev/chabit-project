8ff5a44630e59b038d389ec0a8c931f64270f7bf
Tolga BAYRAK <bayraktolga28@gmail.com>
2025-06-16 21:08:38 +0300

    backend cors

--- node-server/src/controller/auth-controller.js
+++ node-server/src/controller/auth-controller.js
@@ -11,8 +11,8 @@ export default class AuthController {
         try {
             const { email, password } = req.body;
             const result = await this.authService.login(email, password);
-            res.cookie('access_token', result.accessToken, { httpOnly: true });
-            res.cookie('refresh_token', result.refreshToken, { httpOnly: true });
+            res.cookie('access_token', result.accessToken, { httpOnly: true , secure: true, sameSite: "none"});
+            res.cookie('refresh_token', result.refreshToken, { httpOnly: true, secure: true, sameSite: "none" });
             res.status(200).json(result);
         } catch (error) {
             next(error);
