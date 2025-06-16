41c02d521e776a7b15dc7ad68a962c659e57753d
Tolga BAYRAK <bayraktolga28@gmail.com>
2025-06-16 23:41:20 +0300

    s

--- node-server/src/utils/send-email.js
+++ node-server/src/utils/send-email.js
@@ -1,4 +1,7 @@
 import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export async function sendEmail(to, subject, html) {
   const transporter = nodemailer.createTransport({
