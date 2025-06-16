c76bd83bb487b5cac2b7f135f3616259e0ddf12f
Tolga BAYRAK <bayraktolga28@gmail.com>
2025-06-16 19:48:11 +0300

    cors

--- node-server/src/app.js
+++ node-server/src/app.js
@@ -25,7 +25,7 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use('/uploads', express.static('uploads', {
     setHeaders: (res) => {
-        res.set('Access-Control-Allow-Origin', 'http://www.vunqr.com');
+        res.set('Access-Control-Allow-Origin', 'https://www.vunqr.com');
         res.set('Access-Control-Allow-Credentials', 'true');
     }
 }));
