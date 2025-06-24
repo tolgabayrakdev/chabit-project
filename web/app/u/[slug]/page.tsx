9e1c3828ceb526498e6e154748885ab365a4b116
Tolga BAYRAK <bayraktolga28@gmail.com>
2025-06-24 21:35:13 +0300

    s

--- web/app/u/[slug]/page.tsx
+++ web/app/u/[slug]/page.tsx
@@ -2,7 +2,7 @@ import LinkInBioPage from "./LinkInBioClient";
 
 export const metadata = {
   title: "Link In Bio | VunQR",
-  description: "Tüm sosyal medya, iletişim ve tanıtım linklerinizi sade, şık ve etkili bir sayfada birleştirin.",
+  description: "Tüm sosyal medya, iletişim ve tanıtım linklerinizi sade, şık ve etkili bir sayfada birleştirin."
 };
 export default function RegisterPage() {
   return <LinkInBioPage />;
