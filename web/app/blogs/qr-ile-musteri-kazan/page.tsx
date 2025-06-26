fe44005317385e8a3f601bc6a63b1249b8207d3f
Tolga BAYRAK <bayraktolga28@gmail.com>
2025-06-26 23:13:47 +0300

    image

--- web/app/blogs/qr-ile-musteri-kazan/page.tsx
+++ web/app/blogs/qr-ile-musteri-kazan/page.tsx
@@ -95,7 +95,7 @@ export default function BlogPostQrMusteri() {
         </Text>
 
         <Image
-          src="/examples/qr-marketing.png"
+          src="https://images.unsplash.com/photo-1559131397-f94da358f7ca?q=80&w=1169&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           alt="QR Kod ile Pazarlama"
           width={500}
           height={300}
