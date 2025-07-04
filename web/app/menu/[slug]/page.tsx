d406214b5e15635ff99ea09cd5d2ffdcb72c553c
Tolga BAYRAK <bayraktolga28@gmail.com>
2025-07-04 22:53:21 +0300

    store

--- web/app/menu/[slug]/page.tsx
+++ web/app/menu/[slug]/page.tsx
@@ -15,7 +15,6 @@ export async function generateMetadata({ params }: { params: Promise<{ slug: str
       headers: {
         'Content-Type': 'application/json',
       },
-      cache: 'no-store',
     });
 
     if (response.ok) {
