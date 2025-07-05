11ccb6681864c7ebee48d072267b650d2ffee91f
Tolga BAYRAK <bayraktolga28@gmail.com>
2025-07-05 14:49:06 +0300

    wifi

--- web/app/blogs/wifi-qr-code/page.tsx
+++ web/app/blogs/wifi-qr-code/page.tsx
@@ -96,7 +96,7 @@ export default function BlogPostWifiQr() {
         </Text>
 
         <Image
-          src="https://unsplash.com/photos/a-close-up-of-the-wifi-logo-on-the-side-of-a-bus-sOdVYQQo4UU"
+          src="https://images.unsplash.com/photo-1645725677294-ed0843b97d5c?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           alt="WiFi QR Kod"
           width={500}
           height={300}
